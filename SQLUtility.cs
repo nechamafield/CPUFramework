25dd7bc3ceab0db3cc4ba1c0c6253057d3404e1f
nechamafield <nechamafield@gmail.com>
2023-09-07 11:35:14 -0400

    session 25.3

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -51,6 +51,7 @@ namespace CPUFramework
                     }
                 }
             }
+            row.Table.AcceptChanges();
         }
 
         private static DataTable DoExecuteSql (SqlCommand cmd, bool loadtable)
@@ -149,9 +150,11 @@ namespace CPUFramework
 
         private static string ParseConstraintMsg(string msg)
         {
+
             string origmsg = msg;
             string prefix = "ck_";
             string msgend = "";
+            string notnullprefix = "Cannot insert the value NULL into column '";
             if (msg.Contains(prefix) == false)
             {
                 if (msg.Contains("u_"))
@@ -162,6 +165,11 @@ namespace CPUFramework
                 else if (msg.Contains("f_"))
                 {
                     prefix = "f_";
+                }
+                else if (msg.Contains(notnullprefix))
+                {
+                    prefix = notnullprefix;
+                    msgend = " cannot be blank";
                 }
             }
                 if (msg.Contains(prefix))
@@ -216,7 +224,46 @@ namespace CPUFramework
                 }
             }
 
-            public static string GetSQL(SqlCommand cmd)
+            public static int GetValueFromFirstRowAsInt(DataTable dt, string columnname)
+            {
+                int value = 0;
+                if(dt.Rows.Count > 0)
+                {
+                DataRow r = dt.Rows[0];
+                    if (r[columnname] != null && r[columnname] is int)
+                    {
+                    value = (int)r[columnname];
+                    }
+                }
+                return value;
+            }
+
+            public static string GetValueFromFirstRowAsString(DataTable dt, string columnname)
+            {
+                string value = "";
+                if (dt.Rows.Count > 0)
+                {
+                    DataRow r = dt.Rows[0];
+                    if (r[columnname] != null && r[columnname] is string)
+                    {
+                        value = (string)r[columnname];
+                    }
+                }
+                return value;
+            }
+
+        public static bool TableHasChanges(DataTable dt)
+        {
+            bool b = false;
+            if(dt.GetChanges() != null)
+            {
+                b = true;
+            }
+            return b;
+        }
+
+
+        public static string GetSQL(SqlCommand cmd)
             {
                 string val = "";
 #if DEBUG
